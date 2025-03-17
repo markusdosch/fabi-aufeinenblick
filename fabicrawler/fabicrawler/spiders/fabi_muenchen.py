a11e14146b8756d3414841c154401676c2ec20be
Markus Dosch <mail@markusdosch.com>
2025-03-17 14:43:41 +0100

    Fix crawler, continue with website

--- fabicrawler/fabicrawler/spiders/fabi_muenchen.py
+++ fabicrawler/fabicrawler/spiders/fabi_muenchen.py
@@ -5,13 +5,18 @@ from datetime import datetime
 class FabiMuenchenSpider(scrapy.Spider):
     name = "fabi-muenchen"
     allowed_domains = ["fabi-muenchen.de"]
-    start_urls = ["https://www.fabi-muenchen.de/programm/kw/kathaupt/"]
-
+    # start_urls = ["https://www.fabi-muenchen.de/programm/kw/kathaupt/"]
+    
     def start_requests(self):
-        for i, url in enumerate(self.start_urls):
-            yield scrapy.Request(url, meta={"cookiejar": i}, callback=self.parse)
+        return [
+            scrapy.Request(
+                "https://www.fabi-muenchen.de/programm/kw/kathaupt/1?katasfilter[]="+location+"&katasfilter[]=__reset__",
+                meta={"cookiejar": location, "blub": location},
+                callback=self.after_request,
+            ) for location in ["Giesing", "Milbertshofen", "Neuperlach", "Online-Angebot", "Pasing", "Thalkirchen"]
+        ]
 
-    def parse(self, response):
+    def after_request(self, response):
         for entry in response.css(".kw-container > .row > .kw-ue"):
             title = entry.css(".row > div:nth-child(1) > .kw-ue-title > a > b::text").get().strip()
             url = entry.css(".row > div:nth-child(1) > .kw-ue-title > a::attr(href)").get()
@@ -19,7 +24,8 @@ class FabiMuenchenSpider(scrapy.Spider):
             start = extract_date_time_parts(entry.css(".row > div:nth-child(1) > .row:nth-child(2) > div:nth-child(2)::text").get().strip())
             start_date = datetime.strptime(start["date"], '%d.%m.%Y').date() # Parse into Python native date object => will export as ISO format, e.g. 2025-12-01
 
-            location = entry.css(".row > div:nth-child(1) > .row:nth-child(3) > div:nth-child(2)::text").get().strip()
+            location = response.meta["blub"]
+            location_exact = entry.css(".row > div:nth-child(1) > .row:nth-child(3) > div:nth-child(2)::text").get().strip()
 
             price = entry.css(".row > div:nth-child(1) > .row:nth-child(4) > div:nth-child(2)::text").get().strip()
 
@@ -31,9 +37,9 @@ class FabiMuenchenSpider(scrapy.Spider):
                 "title": title,
                 "url": url,
                 "start_weekday": start["weekday"],
-                "start_date": start_date,
-                "start_timerange": start["timerange"],
+                "start": start_date.isoformat() + ", " + start["timerange"],
                 "location": location,
+                "location_exact": location_exact,
                 "price": price,
                 "spots_left": spotsleft
             }
@@ -41,7 +47,7 @@ class FabiMuenchenSpider(scrapy.Spider):
         next_page = response.css(".hauptseite_kurse > div > div.kw-paginationleiste.clearfix > div.text-center.kw-pages > ul > li.active.disabled + li > a::attr(href)").get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, meta={"cookiejar": response.meta["cookiejar"]}, callback=self.parse)
+            yield scrapy.Request(next_page, meta={"cookiejar": response.meta["cookiejar"], "blub": response.meta["blub"]}, callback=self.after_request)
 
 # Sample string: "Mi., 15.01.2025, 19:30 - 20:45 Uhr"
 # Output: Mi., 15.01.2025, 19:30 - 20:45 Uhr
